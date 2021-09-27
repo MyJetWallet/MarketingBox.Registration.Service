24328422b1c4e7dbcd158fa1f85c2f8f6ce24f15
Yuriy Plyaskin <plyaskin.yuri@gmail.com>
2021-09-27 13:53:41 +0300

    Fixed

---
+++ src/MarketingBox.Registration.ServiceBus/Leads/LeadBusUpdateMessage.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.Serialization;
+
+namespace MarketingBox.Registration.Service.Messages.Leads
+{
+    [DataContract]
+    public class LeadBusUpdateMessage
+    {
+        [DataMember(Order = 1)]
+        public string TenantId { get; set; }
+
+        [DataMember(Order = 2)]
+        public long LeadId { get; set; }
+
+        [DataMember(Order = 3)]
+        public string UniqueId { get; set; }
+        
+        [DataMember(Order = 4)]
+        public long Sequence { get; set; }
+
+        [DataMember(Order = 5)]
+        public LeadBusGeneralInfo GeneralInfo { get; set; }
+
+        [DataMember(Order = 6)]
+        public LeadBusRouteInfo RouteInfo { get; set; }
+
+        [DataMember(Order = 7)]
+        public LeadBusAdditionalInfo AdditionalInfo { get; set; }
+
+        [DataMember(Order = 8)]
+        public LeadBusBrandRegistrationInfo RegistrationInfo { get; set; }
+    }
+}
